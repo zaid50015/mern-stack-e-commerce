ef8d0cb578d87772613843db0d7e27144f8141e0
Zaid <mohmmadzaid759@gmail.com>
2023-09-06 14:07:30 +0530

    Issue fixed

--- src/features/common/Modal.js
+++ src/features/common/Modal.js
@@ -69,7 +69,7 @@ const Modal = ({title,message,dangerOption,cancelOption, dangerAction, cancelAct
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
                       >
-                        {title}
+                        {title.title}
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
